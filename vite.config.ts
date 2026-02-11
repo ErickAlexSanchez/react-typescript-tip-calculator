b870e16b823ad807365cdefd5db1ebad1b67f329
ErickAlexSanchez <120741096+ErickAlexSanchez@users.noreply.github.com>
2026-02-10 21:39:22 -0600

    App Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    base: process.env.VITE_BASE_PATH || "/01-local-ecommerce-react",
+    base: process.env.VITE_BASE_PATH || "/react-typescript-tip-calculator",
 })
